55fb49f1927e4a793f55138cec8e74cfd56992e1
Darien Campbell <dacman9@gmail.com>
2024-09-16 12:39:06 -0400

    Tweaked UI design

--- app/src/main/java/com/example/cs501_g2_q3/MainActivity.kt
+++ app/src/main/java/com/example/cs501_g2_q3/MainActivity.kt
@@ -6,11 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.displayCutoutPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -26,9 +30,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cs501_g2_q3.ui.theme.CS501_G2_Q3Theme
 
 class MainActivity : ComponentActivity() {
@@ -38,7 +45,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CS501_G2_Q3Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    AppLayout(modifier=Modifier.padding(innerPadding))
+                    AppLayout(modifier=Modifier.displayCutoutPadding().padding(innerPadding))
                 }
             }
         }
@@ -48,10 +55,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AppLayout(modifier: Modifier = Modifier) {
     var output by remember {mutableStateOf("")}
-    Column(modifier=modifier.fillMaxWidth()) {
+    Column(modifier=modifier.fillMaxWidth().padding(0.dp, 0.dp, 0.dp, 8.dp)) {
         CalculatorOutput(output) {value: String ->
             output = value
         }
+        Spacer(modifier=Modifier.weight(4f))
         ButtonRow() {
             CalcButton("1", 1f) {}
             CalcButton("2", 1f) {}
@@ -82,13 +90,17 @@ fun AppLayout(modifier: Modifier = Modifier) {
 
 @Composable
 fun ColumnScope.ButtonRow(content: @Composable RowScope.() -> Unit) {
-    Row(modifier = Modifier.weight(1f), content=content)
+    Row(modifier = Modifier.weight(1f).padding(PaddingValues(4.dp, 8.dp, 4.dp, 0.dp)), content=content)
 }
 
 @Composable
 fun RowScope.CalcButton(label: String, width: Float, onClick: () -> Unit) {
-    FilledTonalButton(onClick=onClick, modifier=Modifier.weight(width).fillMaxSize()) {
-        Text(label)
+    FilledTonalButton(
+        onClick=onClick,
+        modifier=Modifier.weight(width).padding(4.dp, 0.dp, 4.dp, 0.dp).fillMaxSize(),
+        shape=RoundedCornerShape(16.dp)
+    ) {
+        Text(label, fontSize=24.sp)
     }
 }
 
@@ -96,7 +108,7 @@ fun RowScope.CalcButton(label: String, width: Float, onClick: () -> Unit) {
 fun CalculatorOutput(value: String, onValueChange: (String) -> Unit) {
     TextField(
         modifier = Modifier.fillMaxWidth(),
-        textStyle = TextStyle(textAlign = TextAlign.Right),
+        textStyle = TextStyle(textAlign = TextAlign.Right, fontSize=36.sp, fontFamily= FontFamily.Monospace),
         value = value,
         onValueChange = onValueChange,
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
